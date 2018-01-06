6f8559e19dc99b68d9816956e94a004851fb3250
Tanashi <29756068+GGJohny@users.noreply.github.com>
2018-01-06 15:20:37 +0700

    init

--- opt/panel/index.php
+++ opt/panel/index.php
@@ -32,56 +32,56 @@ if (!empty($_SESSION['user']) && $user = user_info($_SESSION['user'])) {
 			-moz-background-size: cover;
 			background-size: cover;
 		}
-.clock {
-  position: relative;
-  width: 200px;
-  height: 200px;
-  margin: 10px auto;
-	margin-bottom: 30px;
-}
-#hours, #minutes, #seconds {
-  position: absolute;
-  top: 0; left: 0; bottom: 0; right: 0;
-  /* background-color: hsla(20, 75%, 25%, 0.025); */
-  /* border-radius: 50%; */
-}
-#seconds {
-  transform: scale(1.0);
-}
-#minutes {
-  transform: scale(0.85);
-}
-#hours {
-  transform: scale(0.70);
-}
-.date {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-  font-size: 1.2rem;
-  text-align: center;
-  color: hsla(80, 100%, 25%, 0.85);
-  width: 50%;
-}
-sec,min,hour {
-  position: absolute;
-  width: 2%;
-  height: 5%;
-  top: 0; left: 50%;
-  background-color: hsla(80, 100%, 25%, 0.15);
-  border: 1px solid hsla(0, 0%, 100%, 0.05);
-  transform-origin: 0% 1000%;
-  /* transition: all linear 0.5s; */
-}
-.tick {
-  background-color: hsla(80, 100%, 25%, 0.85);
-}
+		.clock {
+		  position: relative;
+		  width: 200px;
+		  height: 200px;
+		  margin: 10px auto;
+			margin-bottom: 30px;
+		}
+		#hours, #minutes, #seconds {
+		  position: absolute;
+		  top: 0; left: 0; bottom: 0; right: 0;
+		  /* background-color: hsla(20, 75%, 25%, 0.025); */
+		  /* border-radius: 50%; */
+		}
+		#seconds {
+		  transform: scale(1.0);
+		}
+		#minutes {
+		  transform: scale(0.85);
+		}
+		#hours {
+		  transform: scale(0.70);
+		}
+		.date {
+		  position: absolute;
+		  top: 50%;
+		  left: 50%;
+		  transform: translate(-50%,-50%);
+		  font-size: 1.2rem;
+		  text-align: center;
+		  color: hsla(80, 100%, 25%, 0.85);
+		  width: 50%;
+		}
+		sec,min,hour {
+		  position: absolute;
+		  width: 2%;
+		  height: 5%;
+		  top: 0; left: 50%;
+		  background-color: hsla(80, 100%, 25%, 0.15);
+		  border: 1px solid hsla(0, 0%, 100%, 0.05);
+		  transform-origin: 0% 1000%;
+		  /* transition: all linear 0.5s; */
+		}
+		.tick {
+		  background-color: hsla(80, 100%, 25%, 0.85);
+		}
 	</style>
 </head>
 <body>
 <noscript>
-	<p class="alert alert-warning"><strong>Enable Javascript:</strong> Javascript is required to use MCHostPanel.</p>
+	<p class="alert alert-warning"><strong>Enable Javascript:</strong> Javascript is required to use MCGG.</p>
 </noscript>
 <form class="modal form-horizontal" action="dashboard.php" method="post">
 <div align="center">
@@ -93,7 +93,7 @@ sec,min,hour {
 	</div>	
 </div>
 	<div class="modal-header" align="center">
-		<h1><i class="fa fa-server" aria-hidden="true"></i> MC<span style="color:green">Host</span>Panel</h1>
+		<h1><i class="fa fa-server" aria-hidden="true"></i> MC<span style="color:gold">GG</span></h1>
 		<h3><?php $count=0; foreach(user_list() as $user) { $count++; } echo $count;?> server đã host</h3>
 	</div>
 	<div class="modal-body">
@@ -127,6 +127,6 @@ sec,min,hour {
 	</div>
 </form>
 <small class="muted pull-left" style="position:absolute;bottom:15px;left:15px;">&copy; <?php echo date('Y'); ?> <a href="https://phpizza.com/">Alan Hardman</a>
- - Re-edit bởi GGJohny<br>From: <?=$_SERVER['REMOTE_ADDR'] ?> - Time: <?=date('d/m/Y') ?></small>
+ - Re-edit bởi GGJohny<br>Guest: <?=$_SERVER['REMOTE_ADDR'] ?> - Time: <?=date('d/m/Y') ?></small>
 	<script src="js/header.js"></script>
 </body>
