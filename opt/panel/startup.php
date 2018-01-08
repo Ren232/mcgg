7d9bb7a9d9c17b29717f7366269addf3e2ee150c
Tanashi <29756068+GGJohny@users.noreply.github.com>
2018-01-08 20:16:30 +0700

    Update startup.php

--- opt/panel/startup.php
+++ opt/panel/startup.php
@@ -1,21 +1,20 @@
 <?php
-require('inc/lib.php');
-if(file_exists('.installed')) {
-  $users = scandir('data/users');
+require dirname(__FILE__).'/inc/lib.php');
+if(file_exists(dirname(__FILE__)."/.installed")) {
+  $users = scandir(dirname(__FILE__).'/data/users');
   foreach($users as $user) {
     $array = explode('.', $user);
     $ext = end($array);
     if($ext == 'json') {
-      $json = json_decode('data/users/'.$user);
+      $json = json_decode(dirname(__FILE__).'/data/users/'.$user);
       // Start the server
-      echo "-----> Starting server for user".$json->user."...";
+      error_log("-----> Starting server for user".$json->user."...");
       server_start($json->user);
     }
   }
 } else {
-  echo "-----> Warning: '.installed' not found! Shutting down... ";
-  exit();
+  exit("-----> Warning: '.installed' not found! Shutting down... ");
 }
 // Exit screen
-exit();
+exit("-----> Completed startup ");
       
