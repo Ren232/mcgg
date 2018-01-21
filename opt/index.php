cc20ccff2c5e9e19f2b950fbfeeb93ed2a3af3a2
Tanashi <29756068+GGJohny@users.noreply.github.com>
2018-01-21 20:11:40 +0700

    Update index.php

--- opt/index.php
+++ opt/index.php
@@ -1,4 +1,8 @@
 <?php
-header("Location: //".$_SERVER['SERVER_NAME']."/panel");
-die();
+if(!file_exists("_index.php")} {
+  header("Location: //".$_SERVER['SERVER_NAME']."/panel");
+  die();
+} else {
+  include("_index.php");
+}
 ?>
