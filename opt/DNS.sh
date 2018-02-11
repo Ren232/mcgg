46df029be3918ab8e7287985175bd7c1e5b79296
Tanashi <29756068+GGJohny@users.noreply.github.com>
2018-02-11 22:27:13 +0700

    Update DNS.sh

--- opt/DNS.sh
+++ opt/DNS.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 function jsonval {
     temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
     echo ${temp##*|}
