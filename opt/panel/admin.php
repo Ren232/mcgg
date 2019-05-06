05de8da473ccebf5956a3ac0587b5193f3e8aed4
Ren232 <45296985+Ren232@users.noreply.github.com>
2019-05-06 18:51:10 +0200

    Update admin.php

--- opt/panel/admin.php
+++ opt/panel/admin.php
@@ -27,7 +27,8 @@ if(isset($_POST['action'])) {
 	// Add new user
 	if ($_POST['action'] == 'user-add')
 		user_add($_POST['user'], $_POST['pass'], $_POST['role'], $_POST['dir'], $_POST['ram'], $_POST['port']);
-		
+		download_version($_POST['version'], $_POST['dir'])
+
 	// Start a server
 	if ($_POST['action'] == 'server-start') {
 		$stu = user_info($_POST['user']);
