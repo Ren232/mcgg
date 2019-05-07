ad6497f557824dfc0e1517ad78735fcd817d30db
Ren232 <45296985+Ren232@users.noreply.github.com>
2019-05-07 18:27:41 +0200

    Update install.php

--- opt/panel/install.php
+++ opt/panel/install.php
@@ -2,7 +2,7 @@
 require_once 'inc/lib.php';
 if (!empty($_POST['user'])) {
 	session_start();
-	user_add($_POST['user'], $_POST['pass'], 'admin', $_POST['dir'], $_POST['ram'], $_POST['port']);
+	user_add($_POST['user'], $_POST['pass'], 'admin', $_POST['dir'], $_POST['ram'], $_POST['port'], $_POST['version']);
 	file_put_contents(".installed", "");
 	$_SESSION['user'] = clean_alphanum($_POST['user']);
 }
