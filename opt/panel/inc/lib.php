d72ce275089fecdacedb3a5fb98fad46f7ef2fcd
Ren232 <45296985+Ren232@users.noreply.github.com>
2019-05-07 17:26:36 +0200

    Update lib.php

--- opt/panel/inc/lib.php
+++ opt/panel/inc/lib.php
@@ -548,6 +548,8 @@ log: ".$_POST['dir']."/ngrok.log \n
 		copy('serverbase/spigot-1.9.4.jar',$_POST['dir'].'/spigot-1.9.4.jar');
 	} else if ($_POST['version'] == '1.8.8') {
 		copy('serverbase/spigot-1.8.8.jar',$_POST['dir'].'/spigot-1.8.8.jar');
+	} else if ($_POST['version'] == '1.7.10') {
+		copy('serverbase/spigot-1.7.10.jar',$_POST['dir'].'/spigot-1.7.10.jar');
 	} else if ($_POST['version'] == 'BC') {
 		copy('serverbase/BungeeCord.jar',$_POST['dir'].'/BungeeCord.jar');
 	}
