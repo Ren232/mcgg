938c6d594fa0a4224b2a681c7fe3c47be4727182
Ren232 <45296985+Ren232@users.noreply.github.com>
2019-06-10 11:37:14 +0200

    Update admin.php

--- opt/panel/admin.php
+++ opt/panel/admin.php
@@ -28,12 +28,10 @@ if(isset($_POST['action'])) {
 	if ($_POST['action'] == 'user-add') 
 		user_add($_POST['user'], $_POST['pass'], $_POST['role'], $_POST['dir'], $_POST['ram'], $_POST['port'], $_POST['version']);
 	// Delete user
-	if ($_POST['action'] == 'user-delete')
-		if (!$user = $_POST['user']) {
-			$stu = user_info($_POST['user'])
+	if ($_POST['action'] == 'user-delete') {
+		if (!$user = $_POST['user'])
+			$usr = user_info($_POST['user'])
 			user_delete($_POST['user'], $stu['dir']);
-		} else
-			return;
 	}
 	// Start a server
 	if ($_POST['action'] == 'server-start') {
