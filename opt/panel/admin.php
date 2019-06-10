7892f2258fcb3c66fd44cf01a059586893675585
Ren232 <45296985+Ren232@users.noreply.github.com>
2019-06-10 12:08:13 +0200

    Update admin.php

--- opt/panel/admin.php
+++ opt/panel/admin.php
@@ -107,8 +107,9 @@ if(isset($_POST['action'])) {
 	<?php } elseif (isset($_POST['action']) && $_POST['action'] == 'user-delete') { ?>
 		<?php if (!$user = user_info($_SESSION[$_POST['user']]) { ?>
 			<p class="alert alert-success pull-right"><i class="icon-ok"></i> User deleted successfully.</p>
-		<?php } else ?>
+		<?php } else { ?>
 			<p class="alert alert-success pull-right"><i class="icon-ok"></i> You can't delete your own account!</p>
+		<?php } ?>
 	<?php } ?>
 	<div class="clearfix"></div>
 	<div class="row-fluid">
