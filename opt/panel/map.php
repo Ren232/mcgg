177aa54d137156d995d3199ac17a61b3f9f049fc
Ren232 <45296985+Ren232@users.noreply.github.com>
2019-06-17 09:03:19 +0200

    Update map.php

--- opt/panel/map.php
+++ opt/panel/map.php
@@ -10,8 +10,9 @@ if (empty($_SESSION['user']) || !$user = user_info($_SESSION['user'])) {
 }
 
 if(isset($_POST['action'])) {
-	if ($_POST['action'] == 'dynmap' && $_POST['user']) {
-		copy('serverbase/dynmap.jar',$_POST['dir'].'/plugins/dynmap.jar');
+	if ($_POST['action'] == 'dynmap') {
+		$stu = user_info($user);
+		copy('serverbase/dynmap.jar',$stu[dir].'/plugins/dynmap.jar');
 	}
 }
 
@@ -42,6 +43,10 @@ if(isset($_POST['action'])) {
 				<iframe id="map" style="width: 100%; height: 600px; border: none;" frameborder="0" src="http://<?php echo $_SERVER['HTTP_HOST']; ?>:<?php echo $port; ?>/"></iframe>
 			<?php } else { ?>
 				<p class="alert alert-danger">No dynmap configuration found.</p>
+				<form action="admin.php" method="post">
+					<input type="hidden" name="action" value="dynmap">
+					<button type="submit" class="btn btn-primary">Install Dynmap</button>
+				</form>
 			<?php } ?>
 			</div>
 		</div>
